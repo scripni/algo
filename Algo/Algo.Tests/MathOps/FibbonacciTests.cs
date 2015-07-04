1378cddbade91ee6b39fe83bd68c1d8b9aded49f
Andrei Scripniciuc <andreisc@outlook.com>
2015-07-04 19:13:01 +0100

    Fibbonacci sequence generator.

---
+++ Algo/Algo.Tests/MathOps/FibbonacciTests.cs
@@ -0,0 +1,28 @@
+﻿using Algo.MathOps;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo.Tests.MathOps
+{
+    [TestClass]
+    public class FibbonacciTests
+    {
+        [TestMethod]
+        public void GenerateFibonacci_SmallNumber_ReturnsExpected()
+        {
+            // arrange
+            List<int> expected = new List<int>() { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+            Fibbonacci f = new Fibbonacci();
+
+            // act
+            List<int> actual = f.CreateFibbonacci(expected.Count);
+
+            // assert
+            Assert.IsTrue(expected.SequenceEqual(actual));
+        }
+    }
+}
---
+++ Algo/Algo/MathOps/Fibbonacci.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Algo.MathOps
+{
+    public class Fibbonacci
+    {
+        public List<int> CreateFibbonacci(int n)
+        {
+            List<int> result = new List<int>();
+            result.Add(0);
+            result.Add(1);
+
+            for (int i = 2; i < n; i++)
+            {
+                result.Add(result[i - 1] + result[i - 2]);
+            }
+
+            return result;
+        }
+    }
+}
