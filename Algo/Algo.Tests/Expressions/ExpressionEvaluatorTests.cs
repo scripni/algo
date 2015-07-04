fd29bed67e3ac4a8b67f5087bd59fbb6b36828e2
Andrei Scripniciuc <andreisc@outlook.com>
2015-07-04 16:42:57 +0100

    added Prefix / Postfix evaluator

---
+++ Algo/Algo.Tests/Expressions/ExpressionEvaluatorTests.cs
@@ -0,0 +1,37 @@
+﻿using Algo.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algo.Tests.Expressions
+{
+    [TestClass]
+    public class ExpressionEvaluatorTests
+    {
+        [TestMethod]
+        public void Prefix_ValidExpression_CorrectResult()
+        {
+            // arrange
+            string input = "-+*23*549";
+            ExpressionParser parser = new ExpressionParser();
+
+            // act
+            int result = parser.Prefix(input);
+
+            // assert
+            Assert.AreEqual(17, result);
+        }
+
+        [TestMethod]
+        public void Postfix_ValidExpression_CorrectResult()
+        {
+            // arrange
+            string input = "23*54*+9-";
+            ExpressionParser parser = new ExpressionParser();
+
+            // act
+            int result = parser.Postfix(input);
+
+            // assert
+            Assert.AreEqual(17, result);
+        }
+    }
+}
---
+++ Algo/Algo/Expressions/ExpressionParser.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+
+namespace Algo.Expressions
+{
+    public class ExpressionParser
+    {
+        public int Prefix(string e)
+        {
+            Stack<int> s = new Stack<int>();
+
+            for (int i = e.Length - 1; i >= 0; i--)
+            {
+                char c = e[i];
+                if (IsOperator(c))
+                {
+                    int a = s.Pop();
+                    int b = s.Pop();
+                    switch (c)
+                    {
+                        case '+':
+                            s.Push(a + b);
+                            break;
+                        case '-':
+                            s.Push(a - b);
+                            break;
+                        case '*':
+                            s.Push(a * b);
+                            break;
+                        case '/':
+                            s.Push(a / b);
+                            break;
+                    }
+                }
+                else
+                {
+                    s.Push(c - '0');
+                }
+            }
+
+            return s.Pop();
+        }
+
+        public int Postfix(string e)
+        {
+            Stack<int> s = new Stack<int>();
+            foreach (char c in e)
+            {
+                if (IsOperator(c))
+                {
+                    int b = s.Pop();
+                    int a = s.Pop();
+                    switch (c)
+                    {
+                        case '+':
+                            s.Push(a + b);
+                            break;
+                        case '-':
+                            s.Push(a - b);
+                            break;
+                        case '*':
+                            s.Push(a * b);
+                            break;
+                        case '/':
+                            s.Push(a / b);
+                            break;
+                    }
+                }
+                else
+                {
+                    s.Push(c - '0');
+                }
+            }
+
+            return s.Pop();
+        }
+
+        private static bool IsOperator(char c)
+        {
+            return c == '+' || c == '-' || c == '*' || c == '/';
+        }
+    }
+}
