966ebda101ad9bc4221927932781f7cc4c45428e
Andrei Scripniciuc <andreisc@outlook.com>
2015-07-04 19:49:45 +0100

    palindrome ignore puctuation, capitalization

---
+++ Algo/Algo.Tests/Strings/PalindromeTests.cs
@@ -0,0 +1,53 @@
+﻿using Algo.Strings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algo.Tests.Strings
+{
+    [TestClass]
+    public class PalindromeTests
+    {
+        [TestMethod]
+        public void IsPalindrome_OnlyLowercaseChars_ReturnsTrue()
+        {
+            // arrange
+            string palindrome1 = "abccba";
+            string palindrome2 = "abcba";
+            Palindrome p = new Palindrome();
+
+            // act
+            // assert
+            Assert.IsTrue(p.IsPalindromeLoose(palindrome1));
+            Assert.IsTrue(p.IsPalindromeLoose(palindrome2));
+        }
+
+
+        [TestMethod]
+        public void IsPalindrome_LowerUpperChars_ReturnsTrue()
+        {
+            // arrange
+            string palindrome1 = "abCcba";
+            string palindrome2 = "aBcbA";
+            Palindrome p = new Palindrome();
+
+            // act
+            // assert
+            Assert.IsTrue(p.IsPalindromeLoose(palindrome1));
+            Assert.IsTrue(p.IsPalindromeLoose(palindrome2));
+        }
+
+
+        [TestMethod]
+        public void IsPalindrome_LowerUpperPunctuation_ReturnsTrue()
+        {
+            // arrange
+            string palindrome1 = "ab,C.c  ba";
+            string palindrome2 = "a\\B,cb|A";
+            Palindrome p = new Palindrome();
+
+            // act
+            // assert
+            Assert.IsTrue(p.IsPalindromeLoose(palindrome1));
+            Assert.IsTrue(p.IsPalindromeLoose(palindrome2));
+        }
+    }
+}
---
+++ Algo/Algo/Strings/Palindrome.cs
@@ -0,0 +1,29 @@
+﻿namespace Algo.Strings
+{
+    public class Palindrome
+    {
+        public bool IsPalindromeLoose(string s)
+        {
+            for (int i = 0, j = s.Length - 1; i >= j; i++, j--)
+            {
+                while (i < 'a' || i > 'Z')
+                {
+                    i++;
+                }
+
+                char a = char.ToLowerInvariant(s[i]);
+
+                while (j < 'a' || j > 'Z')
+                {
+                    j--;
+                }
+
+                char b = char.ToLowerInvariant(s[j]);
+
+                if (a != b) return false;
+            }
+
+            return true;
+        }
+    }
+}
