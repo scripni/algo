a49f8add21fe0185075b28846968d94701ff9502
Andrei Scripniciuc <andreisc@outlook.com>
2015-07-05 13:09:09 +0100

    non-recursive implementations for tree traversal

--- Algo/Algo.Tests/Trees/TreeTraversalTests.cs
+++ Algo/Algo.Tests/Trees/TreeTraversalTests.cs
@@ -25,7 +25,7 @@ namespace Algo.Tests.Trees
             TreeTraversal traversal = new TreeTraversal();
 
             // act
-            BinaryTreeNode[] levelFirst = traversal.LevelFirstTraversal(root).ToArray();
+            BinaryTreeNode[] levelFirst = traversal.LevelOrder(root).ToArray();
 
             // assert
             Assert.AreEqual(levelFirst[0].Value[0], '0');
@@ -37,6 +37,99 @@ namespace Algo.Tests.Trees
             Assert.AreEqual(levelFirst[6].Value[0], '2');
             Assert.AreEqual(levelFirst[7].Value[0], '3');
             Assert.AreEqual(levelFirst[8].Value[0], '4');
+        }
+
+
+        [TestMethod]
+        public void InOrderTraversal_ValidTree_CorrectSequence()
+        {
+            // arrange
+            BinaryTreeNode root = new BinaryTreeNode("0-root");
+            root.Left = new BinaryTreeNode("1-left");
+            root.Right = new BinaryTreeNode("1-right");
+            root.Left.Left = new BinaryTreeNode("2-left-left");
+            root.Left.Right = new BinaryTreeNode("2-left-right");
+            root.Right.Left = new BinaryTreeNode("2-right-left");
+            root.Right.Right = new BinaryTreeNode("2-right-right");
+            root.Right.Right.Left = new BinaryTreeNode("3-right-right-left");
+            root.Right.Right.Left.Right = new BinaryTreeNode("4-right-right-left-right");
+            TreeTraversal traversal = new TreeTraversal();
+
+            // act
+            BinaryTreeNode[] levelFirst = traversal.InOrder(root).ToArray();
+
+            // assert
+            Assert.AreEqual(levelFirst[0].Value, "2-left-left");
+            Assert.AreEqual(levelFirst[1].Value, "1-left");
+            Assert.AreEqual(levelFirst[2].Value, "2-left-right");
+            Assert.AreEqual(levelFirst[3].Value, "0-root");
+            Assert.AreEqual(levelFirst[4].Value, "2-right-left");
+            Assert.AreEqual(levelFirst[5].Value, "1-right");
+            Assert.AreEqual(levelFirst[6].Value, "3-right-right-left");
+            Assert.AreEqual(levelFirst[7].Value, "4-right-right-left-right");
+            Assert.AreEqual(levelFirst[8].Value, "2-right-right");
+        }
+
+
+        [TestMethod]
+        public void PreOrderTraversal_ValidTree_CorrectSequence()
+        {
+            // arrange
+            BinaryTreeNode root = new BinaryTreeNode("0-root");
+            root.Left = new BinaryTreeNode("1-left");
+            root.Right = new BinaryTreeNode("1-right");
+            root.Left.Left = new BinaryTreeNode("2-left-left");
+            root.Left.Right = new BinaryTreeNode("2-left-right");
+            root.Right.Left = new BinaryTreeNode("2-right-left");
+            root.Right.Right = new BinaryTreeNode("2-right-right");
+            root.Right.Right.Left = new BinaryTreeNode("3-right-right-left");
+            root.Right.Right.Left.Right = new BinaryTreeNode("4-right-right-left-right");
+            TreeTraversal traversal = new TreeTraversal();
+
+            // act
+            BinaryTreeNode[] levelFirst = traversal.PreOrder(root).ToArray();
+
+            // assert
+            Assert.AreEqual(levelFirst[0].Value, "0-root");
+            Assert.AreEqual(levelFirst[1].Value, "1-left");
+            Assert.AreEqual(levelFirst[2].Value, "2-left-left");
+            Assert.AreEqual(levelFirst[3].Value, "2-left-right");
+            Assert.AreEqual(levelFirst[4].Value, "1-right");
+            Assert.AreEqual(levelFirst[5].Value, "2-right-left");
+            Assert.AreEqual(levelFirst[6].Value, "2-right-right");
+            Assert.AreEqual(levelFirst[7].Value, "3-right-right-left");
+            Assert.AreEqual(levelFirst[8].Value, "4-right-right-left-right");
+        }
+
+
+        [TestMethod]
+        public void PostOrderTraversal_ValidTree_CorrectSequence()
+        {
+            // arrange
+            BinaryTreeNode root = new BinaryTreeNode("0-root");
+            root.Left = new BinaryTreeNode("1-left");
+            root.Right = new BinaryTreeNode("1-right");
+            root.Left.Left = new BinaryTreeNode("2-left-left");
+            root.Left.Right = new BinaryTreeNode("2-left-right");
+            root.Right.Left = new BinaryTreeNode("2-right-left");
+            root.Right.Right = new BinaryTreeNode("2-right-right");
+            root.Right.Right.Left = new BinaryTreeNode("3-right-right-left");
+            root.Right.Right.Left.Right = new BinaryTreeNode("4-right-right-left-right");
+            TreeTraversal traversal = new TreeTraversal();
+
+            // act
+            BinaryTreeNode[] levelFirst = traversal.PostOrder(root).ToArray();
+
+            // assert
+            Assert.AreEqual(levelFirst[0].Value, "2-left-left");
+            Assert.AreEqual(levelFirst[1].Value, "2-left-right");
+            Assert.AreEqual(levelFirst[2].Value, "1-left");
+            Assert.AreEqual(levelFirst[3].Value, "2-right-left");
+            Assert.AreEqual(levelFirst[4].Value, "4-right-right-left-right");
+            Assert.AreEqual(levelFirst[5].Value, "3-right-right-left");
+            Assert.AreEqual(levelFirst[6].Value, "2-right-right");
+            Assert.AreEqual(levelFirst[7].Value, "1-right");
+            Assert.AreEqual(levelFirst[8].Value, "0-root");
         }
     }
 }
--- Algo/Algo/Trees/TreeTraversal.cs
+++ Algo/Algo/Trees/TreeTraversal.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Algo.Trees
 {
     public class TreeTraversal
     {
-        public IEnumerable<BinaryTreeNode> LevelFirstTraversal(BinaryTreeNode root)
+        public IEnumerable<BinaryTreeNode> LevelOrder(BinaryTreeNode root)
         {
             Queue<BinaryTreeNode> nodes = new Queue<BinaryTreeNode>();
             nodes.Enqueue(root);
@@ -25,6 +26,113 @@ namespace Algo.Trees
 
                 yield return node;
             }
+        }
+
+
+        public IEnumerable<BinaryTreeNode> InOrder(BinaryTreeNode node)
+        {
+            if (node == null) { return Enumerable.Empty<BinaryTreeNode>(); }
+            return InOrder(node.Left)
+                .Union(new[] { node })
+                .Union(InOrder(node.Right));
+
+            // non-recursive
+            //Stack<BinaryTreeNode> nodes = new Stack<BinaryTreeNode>();
+            //BinaryTreeNode current = node;
+
+            //while (nodes.Count > 0 || current != null)
+            //{
+            //    while (current != null)
+            //    {
+            //        nodes.Push(current);
+            //        current = current.Left;
+            //    }
+
+            //    yield return nodes.Peek();
+
+            //    current = nodes.Pop().Right;
+            //}
+        }
+
+
+        public IEnumerable<BinaryTreeNode> PreOrder(BinaryTreeNode node)
+        {
+            if (node == null) { return Enumerable.Empty<BinaryTreeNode>(); }
+
+            return new[] { node }
+                .Union(PreOrder(node.Left))
+                .Union(PreOrder(node.Right));
+
+            // non-recursive
+            //Stack<BinaryTreeNode> nodes = new Stack<BinaryTreeNode>();
+            //nodes.Push(node);
+            //while (nodes.Count > 0)
+            //{
+            //    BinaryTreeNode current = nodes.Pop();
+            //    yield return current;
+            //    if (current.Right != null)
+            //    {
+            //        nodes.Push(current.Right);
+            //    }
+
+            //    if (current.Left != null)
+            //    {
+            //        nodes.Push(current.Left);
+            //    }
+            //}
+        }
+
+
+        public IEnumerable<BinaryTreeNode> PostOrder(BinaryTreeNode node)
+        {
+            if (node == null) { return Enumerable.Empty<BinaryTreeNode>(); }
+
+            return PostOrder(node.Left).Union(PostOrder(node.Right)).Union(new[] { node });
+
+            // non-recursive
+            //Stack<BinaryTreeNode> nodes = new Stack<BinaryTreeNode>();
+            //nodes.Push(node);
+            //BinaryTreeNode previous = null;
+            //while (nodes.Count > 0)
+            //{
+            //    BinaryTreeNode current = nodes.Peek();
+
+            //    // travel down
+            //    if (previous == null || previous.Left == current || previous.Right == current)
+            //    {
+            //        if (current.Left != null)
+            //        {
+            //            nodes.Push(current.Left);
+            //        }
+            //        else if (current.Right != null)
+            //        {
+            //            nodes.Push(current.Right);
+            //        }
+            //        else
+            //        {
+            //            yield return nodes.Pop();
+            //        }
+            //    }
+            //    // travel up from the left
+            //    else if (current.Left == previous)
+            //    {
+            //        if (current.Right != null)
+            //        {
+            //            nodes.Push(current.Right);
+            //        }
+            //        else
+            //        {
+            //            yield return nodes.Pop();
+            //        }
+            //    }
+            //    // travel up from the right
+            //    else if (current.Right == previous)
+            //    {
+            //        yield return nodes.Pop();
+            //    }
+
+            //    previous = current;
+            //}
         }
     }
 }
