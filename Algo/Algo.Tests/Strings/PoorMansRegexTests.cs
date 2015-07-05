d1ebc2f9b2f1b1b03df14313c5e41c972318c385
Andrei Scripniciuc <andreisc@outlook.com>
2015-07-05 10:59:37 +0100

    poor man's regex

---
+++ Algo/Algo.Tests/Strings/PoorMansRegexTests.cs
@@ -0,0 +1,104 @@
+﻿using Algo.Strings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo.Tests.Strings
+{
+    [TestClass]
+    public class PoorMansRegexTests
+    {
+        [TestMethod]
+        public void Match_StartOfString_FindsMatch()
+        {
+            // arrange
+            string input = "andrei scripniciuc";
+            string pattern = "andrei";
+            PoorMansRegex r = new PoorMansRegex();
+
+            // act
+            string actual = r.Match(input, pattern);
+
+            // assert
+            Assert.AreEqual(pattern, actual);
+        }
+
+        [TestMethod]
+        public void Match_EndOfString_FindsMatch()
+        {
+            // arrange
+            string input = "andrei scripniciuc";
+            string pattern = "scripniciuc";
+            PoorMansRegex r = new PoorMansRegex();
+
+            // act
+            string actual = r.Match(input, pattern);
+
+            // assert
+            Assert.AreEqual(pattern, actual);
+        }
+
+        [TestMethod]
+        public void Match_MiddleOfString_FindsMatch()
+        {
+            // arrange
+            string input = "dubl, andrei scripniciuc, dublin, ireland";
+            string pattern = "dublin";
+            PoorMansRegex r = new PoorMansRegex();
+
+            // act
+            string actual = r.Match(input, pattern);
+
+            // assert
+            Assert.AreEqual(pattern, actual);
+        }
+
+        [TestMethod]
+        public void Match_IncludesZeroOrMoreMatch_FindsMatch()
+        {
+            // arrange
+            string input = "foo bar";
+            string pattern = "fo*";
+            PoorMansRegex r = new PoorMansRegex();
+
+            // act
+            string actual = r.Match(input, pattern);
+
+            // assert
+            Assert.AreEqual("foo", actual);
+        }
+
+        [TestMethod]
+        public void Match_IncludesZeroOrMoreMatchWithZeroMatch_FindsMatch()
+        {
+            // arrange
+            string input = "foo bar";
+            string pattern = "fooa*";
+            PoorMansRegex r = new PoorMansRegex();
+
+            // act
+            string actual = r.Match(input, pattern);
+
+            // assert
+            Assert.AreEqual("foo", actual);
+        }
+
+        [TestMethod]
+        public void Match_IncludesDotMatch_FindsMatch()
+        {
+            // arrange
+            string input = "foo bar";
+            string pattern = "foo.bar";
+            PoorMansRegex r = new PoorMansRegex();
+
+            // act
+            string actual = r.Match(input, pattern);
+
+            // assert
+            Assert.AreEqual("foo bar", actual);
+        }
+    }
+}
---
+++ Algo/Algo/Strings/PoorMansRegex.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Algo.Strings
+{
+    public class PoorMansRegex
+    {
+        public string Match(string input, string pattern)
+        {
+            // build search stack
+            Stack<SearchItem> toSearch = new Stack<SearchItem>();
+            for (int i = pattern.Length - 1; i >= 0; i--)
+            {
+                SearchItem searchItem = new SearchItem();
+                if (pattern[i] == '*')
+                {
+                    i--;
+                    searchItem.MatchZeroOrMore = true;
+                }
+
+                searchItem.Character = pattern[i];
+                toSearch.Push(searchItem);
+            }
+
+            Stack<SearchItem> found = new Stack<SearchItem>();
+            StringBuilder result = new StringBuilder();
+            int inputIndex = 0;
+
+            // search for pattern
+            while (toSearch.Count > 0 && inputIndex < input.Length)
+            {
+                for (int i = inputIndex; i < input.Length && toSearch.Count > 0; i++)
+                {
+                    SearchItem current = toSearch.Peek();
+
+                    if (current.MatchZeroOrMore)
+                    {
+                        while (current.Character == input[i])
+                        {
+                            result.Append(input[i++]);
+                        }
+
+                        found.Push(toSearch.Pop());
+                    }
+                    else if (current.Character == '.' || current.Character == input[i])
+                    {
+                        result.Append(input[i]);
+                        found.Push(toSearch.Pop());
+                    }
+                    else
+                    {
+                        inputIndex++;
+                        while (found.Count > 0) toSearch.Push(found.Pop());
+                        result.Length = 0;
+                        break;
+                    }
+                }
+            }
+
+            if (toSearch.Count == 0) return result.ToString();
+            else return null;
+        }
+    }
+
+    public struct SearchItem
+    {
+        public char Character;
+
+        public bool MatchZeroOrMore;
+    }
+}
