63fcd2e50867a13ad4c19966ed825cf0f3a61cb0
Andrei Scripniciuc <andreisc@outlook.com>
2015-07-05 14:17:25 +0100

    convert tree to double circular linked list

---
+++ Algo/Algo.Tests/Lists/ListOperationTests.cs
@@ -0,0 +1,41 @@
+﻿using Algo.Lists;
+using Algo.Trees;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algo.Tests.Lists
+{
+    [TestClass]
+    public class ListOperationTests
+    {
+        [TestMethod]
+        public void ConvertTree_ValidTree_ConvertsToCircularLinkedList()
+        {
+            // arrange
+            BinaryTreeNode root = new BinaryTreeNode("0-root");
+            root.Left = new BinaryTreeNode("1-left");
+            root.Right = new BinaryTreeNode("1-right");
+            root.Left.Left = new BinaryTreeNode("2-left-left");
+            root.Left.Right = new BinaryTreeNode("2-left-right");
+            root.Right.Left = new BinaryTreeNode("2-right-left");
+            root.Right.Right = new BinaryTreeNode("2-right-right");
+            root.Right.Right.Left = new BinaryTreeNode("3-right-right-left");
+            root.Right.Right.Left.Right = new BinaryTreeNode("4-right-right-left-right");
+            ListOperations ops = new ListOperations();
+
+            // act
+            ListOperations.LinkedListNode actual = ops.ConvertTree(root);
+
+            // assert
+            Assert.AreEqual(root.Value, actual.Value);
+            Assert.AreEqual(root.Left.Value, actual.Next.Value);
+            Assert.AreEqual(root.Right.Value, actual.Next.Next.Value);
+            Assert.AreEqual(root.Left.Left.Value, actual.Next.Next.Next.Value);
+            Assert.AreEqual(root.Left.Right.Value, actual.Next.Next.Next.Next.Value);
+            Assert.AreEqual(root.Right.Left.Value, actual.Next.Next.Next.Next.Next.Value);
+            Assert.AreEqual(root.Right.Right.Value, actual.Next.Next.Next.Next.Next.Next.Value);
+            Assert.AreEqual(root.Right.Right.Left.Value, actual.Next.Next.Next.Next.Next.Next.Next.Value);
+            Assert.AreEqual(root.Right.Right.Left.Right.Value, actual.Next.Next.Next.Next.Next.Next.Next.Next.Value);
+            Assert.AreEqual(actual, actual.Next.Next.Next.Next.Next.Next.Next.Next.Next);
+        }
+    }
+}
---
+++ Algo/Algo/Lists/ListOperations.cs
@@ -0,0 +1,82 @@
+﻿using Algo.Trees;
+using System.Collections.Generic;
+
+namespace Algo.Lists
+{
+    public class ListOperations
+    {
+        public LinkedList<T> ReverseLinkedList<T>(LinkedList<T> list)
+        {
+            Stack<LinkedListNode<T>> s = new Stack<LinkedListNode<T>>();
+            LinkedListNode<T> n = list.First;
+            while (n != null)
+            {
+                s.Push(n);
+                n = n.Next;
+            }
+
+            LinkedList<T> result = new LinkedList<T>();
+            while (s.Count > 0)
+            {
+                result.AddLast(s.Pop());
+            }
+
+            return result;
+        }
+
+
+        public LinkedListNode ConvertTree(BinaryTreeNode n)
+        {
+            // level order
+            Queue<BinaryTreeNode> nodes = new Queue<BinaryTreeNode>();
+            nodes.Enqueue(n);
+
+            LinkedListNode previous = null;
+            LinkedListNode first = null;
+            LinkedListNode current = null;
+
+            while (nodes.Count > 0)
+            {
+                BinaryTreeNode currentTreeNode = nodes.Dequeue();
+
+                current = new LinkedListNode();
+                current.Value = currentTreeNode.Value;
+                if (previous != null)
+                {
+                    current.Previous = previous;
+                    previous.Next = current;
+                }
+
+                if (first == null)
+                {
+                    first = current;
+                }
+
+                previous = current;
+
+                if (currentTreeNode.Left != null)
+                {
+                    nodes.Enqueue(currentTreeNode.Left);
+                }
+
+                if (currentTreeNode.Right != null)
+                {
+                    nodes.Enqueue(currentTreeNode.Right);
+                }
+            }
+
+            first.Previous = current;
+            current.Next = first;
+
+            return first;
+        }
+
+        public class LinkedListNode
+        {
+            public string Value;
+
+            public LinkedListNode Previous;
+            public LinkedListNode Next;
+        }
+    }
+}
